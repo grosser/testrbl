5f0e994f5571bde15fbaf4c213b813f080ec8aa6
grosser <grosser.michael@gmail.com>
2012-09-23 19:47:21 -0700

    refactor a bit and remove minitest or other breaking on single quote

--- lib/testrbl.rb
+++ lib/testrbl.rb
@@ -80,28 +80,28 @@ module Testrbl
 
   def self.pattern_from_line(line)
     PATTERNS.each do |r|
-      if line =~ r
-        whitespace, method, test_name = $1, $2, $3
-        regex = Regexp.escape(test_name).gsub("'",".").gsub("\\ "," ").gsub(INTERPOLATION, ".*")
-
-        if method == "should"
-          optional_test_name = "(?:\(.*\))?"
-          regex = "#{method} #{regex}\. #{optional_test_name}$"
-        elsif method == "test"
-          # test "xxx -_ yyy"
-          # test-unit:     "test: xxx -_ yyy"
-          # activesupport: "test_xxx_-__yyy"
-          regex = "^test(: |_)#{regex.gsub(" ", ".")}$"
-        elsif method == "it"
-          regex = "^test_\\d+_#{test_name}$"
-        end
-
-        return [
-          whitespace,
-          regex
-        ]
-      end
+      next unless line =~ r
+      whitespace, method, test_name = $1, $2, $3
+      return [whitespace, pattern_from_match(method, test_name)]
     end
     nil
+  end
+
+  def self.pattern_from_match(method, test_name)
+    regex = Regexp.escape(test_name).gsub("\\ "," ").gsub(INTERPOLATION, ".*")
+
+    if method == "should"
+      optional_test_name = "(?:\(.*\))?"
+      regex = "#{method} #{regex}\. #{optional_test_name}$"
+    elsif method == "test"
+      # test "xxx -_ yyy"
+      # test-unit:     "test: xxx -_ yyy"
+      # activesupport: "test_xxx_-__yyy"
+      regex = "^test(: |_)#{regex.gsub(" ", ".")}$"
+    elsif method == "it"
+      regex = "^test_\\d+_#{test_name}$"
+    end
+
+    regex.gsub("'", ".")
   end
 end
